c955532f7bb47caa6421e39d5e3a0eee167af259
Marc Bresson <marco.bresson@gmail.com>
2023-03-20 18:08:20 +0000

    build: setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="TACTUS - yolov7",
+    version="0.0.0",
+    description="Yolov7 library for tactus",
+    packages=['yolov7'],
+    classifiers=[
+        "Development Status :: 5 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.9",
+)
