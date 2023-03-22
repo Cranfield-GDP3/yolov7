668eb5a3715dc5638fd62b80160d7c7ded40c3d3
Marc Bresson <marco.bresson@gmail.com>
2023-03-22 15:44:56 +0000

    fix: remove general matplotlib config

--- tactus_yolov7/utils/plots.py
+++ tactus_yolov7/utils/plots.py
@@ -21,10 +21,6 @@ from scipy.signal import butter, filtfilt
 from .general import xywh2xyxy, xyxy2xywh
 from .metrics import fitness
 
-# Settings
-matplotlib.rc('font', **{'size': 11})
-matplotlib.use('Agg')  # for writing to files only
-
 
 def color_list():
     # Return first 10 plt colors as (r,g,b) https://stackoverflow.com/questions/51350872/python-from-color-name-to-rgb
